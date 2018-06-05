4abf591b79023a099e816e98f0a2be2f6483dea9
Jacob Lindholm <jacob.lindholm@oracle.com>
2018-06-05 10:22:01 -0400

    added uninstall

--- run.sh
+++ run.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# This step will install the Aura microservice platform as part of a Wercker pipeline
+#
+# Parameters:
+#   KUBERNETES_MASTER - specifies the full address of the master node of the target kubernetes cluster
+#                       including protocol and port.  ie https://<url>:6443
+#   KUBERNETES_TOKEN  - the authentication token for a user with permission to install into the cluster
+#                       this can be taken from the kubectl config file
+#   PULL_DEPENDENCIES - "true" if the step is being run from an unpublished source
+#                       this will cause the step to download kubectl and helm as part of the pipeline
+#                       the published step will already include these in its image
+#   INSTALL_TYPE      - "install" - the default, will install the Aura platform
+#                       "uninstall" - will uninstall the Aura platform
+#
+
 kubectl="$WERCKER_STEP_ROOT/kubectl"
 
 # for local testing
@@ -10,7 +24,8 @@ kubecall() {
   kubecall_server="$2"
   kubecall_token="$3"
 
-  display_version
+  echo "INFO: Running kubectl version:"
+  "$kubectl" version --client
 
   echo "kubecall called with: "
   echo "         kubecall_command = $kubecall_command"
@@ -56,27 +71,13 @@ kubecall() {
   #  args="$args --timeout=\"$WERCKER_KUBECTL_TIMEOUT\""
   #fi
 
-  WERCKER_AURA_DEBUG=true
-  ##info "Running kubctl command"
-  echo "INFO: Running kubctl command"
-  if [ "$WERCKER_AURA_DEBUG" = "true" ]; then
-    ##info "kubectl $global_args $raw_global_args $cmd $args $raw_args"
-    echo "INFO: kubectl $global_args $raw_global_args $cmd $args $raw_args"
-  fi
-
   echo  eval "$kubectl" "$global_args" "$raw_global_args" "$cmd" "$args" "$raw_args"
   eval "$kubectl" "$global_args" "$raw_global_args" "$cmd" "$args" "$raw_args"
 
-
 }
 
-display_version() {
-  # info "Running kubectl version:"
-  echo "INFO: Running kubectl version:"
-  "$kubectl" version --client
-  echo ""
-}
 
+# This is used by helm calls
 generate_kubeconfig() {
     master="$1"
     token="$2"
@@ -106,11 +107,6 @@ generate_kubeconfig() {
               token: ${token}
     " > /root/.kube/config
 
-}
-
-
-save_kubeconfig() {
-  echo
 }
 
 
@@ -148,24 +144,22 @@ pull_helm() {
 }
 
 
-
+# main method starts here 
 main() {
-
   server="$WERCKER_STEP_AURA_SERVER"
   token="$WERCKER_STEP_AURA_TOKEN"
 
-  echo "main: WERCKER_STEP_AURA_SERVER - $WERCKER_STEP_AURA_SERVER"
-  echo "main: WERCKER_STEP_AURA_TOKEN - $WERCKER_STEP_AURA_TOKEN"
-
-  echo "main: PULL_DEPENDENCIES - $PULL_DEPENDENCIES"
-
-  generate_kubeconfig "$server" "$token" "cluster1"
-
-  echo "Created kubeconfig:"
-  cat /root/.kube/config
+  # echo "INFO: WERCKER_STEP_AURA_SERVER - $WERCKER_STEP_AURA_SERVER"
+  # echo "INFO: WERCKER_STEP_AURA_TOKEN - $WERCKER_STEP_AURA_TOKEN"
+  echo "INFO: PULL_DEPENDENCIES - $PULL_DEPENDENCIES"
+  echo "INFO: INSTALL_TYPE - $INSTALL_TYPE"
 
   # this part should alternatively take a pasted kubeconfig 
   # and make kubecall just use the right context in the new file
+  generate_kubeconfig "$server" "$token" "cluster1"
+  echo "Created kubeconfig:"
+  cat /root/.kube/config
+
 
   # for running an unpublished step, kubectl and helm must be pulled during run
   # for a published step, they will be installed as part of the step's image
@@ -176,13 +170,7 @@ main() {
       pull_helm
   fi
 
-  echo
-  echo "  Contents of $WERCKER_STEP_ROOT"
-  ls -R $WERCKER_STEP_ROOT
-  echo
- 
-  # kubecall "get pods --all-namespaces" "$server" "$token"
- 
+
   echo "Create aura namespace"
   # don't fail here if aura namespace exists
   kubecall "create namespace aura" "$server" "$token" || true
@@ -193,24 +181,29 @@ main() {
   echo "Set up Istio injection"
   kubecall "label namespace default istio-injection=enabled --overwrite=true" "$server" "$token" 
 
-  echo "Delete previous install job"
-  kubecall "delete job -n aura install-aura --ignore-not-found" "$server" "$token"
-  kubecall "delete job -n aura install-events-broker --ignore-not-found" "$server" "$token"
+  if [ "$INSTALL_TYPE" = "uninstall" ] ; then
+      echo "Uninstalling Aura"
 
-  # this should come from public public storage
-  echo "Apply the installer job"
-  kubecall "apply -f ${WERCKER_STEP_ROOT}/aura-installer-job.yml" "$server" "$token"
+      echo "Delete previous install job"
+      kubecall "delete job -n aura uninstall-aura-full --ignore-not-found" "$server" "$token"
 
+      echo "Apply the uninstaller job"
+      kubecall "apply -f ${WERCKER_STEP_ROOT}/aura-uninstaller-job-full.yml" "$server" "$token"
+  else
+      echo "Installing Aura" 
+
+      echo "Delete previous install job"
+      #kubecall "delete job -n aura install-aura --ignore-not-found" "$server" "$token"
+      kubecall "delete job -n aura install-aura-full --ignore-not-found" "$server" "$token"
+      #kubecall "delete job -n aura install-events-broker --ignore-not-found" "$server" "$token"
+
+      # this should come from public public storage
+      echo "Apply the installer job"
+      kubecall "apply -f ${WERCKER_STEP_ROOT}/aura-installer-job-full.yml" "$server" "$token"
+  fi
 
 }
 
 main;
-
-
-
-
-
-
-
 
 
